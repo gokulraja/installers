80d852b21269fcbf46e898cb4aeeea5845a69af4
gokulraja <gokulrajad@gmail.com>
2025-06-25 15:13:37 +0530

    Update Psinstall.ps1
    added winget apps

--- Psinstall.ps1
+++ Psinstall.ps1
@@ -1,12 +1,13 @@
+#Install Chocolatey
 Get-ExecutionPolicy
 Run Get-ExecutionPolicy. If it returns Restricted, then run Set-ExecutionPolicy AllSigned or Set-ExecutionPolicy Bypass -Scope Process.
-
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 
 choco install nvm --version 1.2.2 -y
 
+OR------------- Check if winget is already there
 
 Get winget
 Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
@@ -17,3 +18,5 @@ https://winstall.app/
 
 winget install --id=Microsoft.Teams  -e
 winget install --id=JetBrains.IntelliJIDEA.Community  -e
+winget install --id=CoreyButler.NVMforWindows  -e
+winget install --id=OpenJS.NodeJS.LTS  -e

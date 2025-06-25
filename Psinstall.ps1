d8196fbec0b952ff184c5629a4d3a92440b5e5e6
gokulraja <gokulrajad@gmail.com>
2025-06-25 15:09:18 +0530

    Create Psinstall.ps1

---
+++ Psinstall.ps1
@@ -0,0 +1,19 @@
+Get-ExecutionPolicy
+Run Get-ExecutionPolicy. If it returns Restricted, then run Set-ExecutionPolicy AllSigned or Set-ExecutionPolicy Bypass -Scope Process.
+
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+choco install nvm --version 1.2.2 -y
+
+
+Get winget
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+
+Read: https://code.adonline.id.au/windows-package-manager-winget-cheat-sheet/#listing-whats-installed
+https://winstall.app/
+
+
+winget install --id=Microsoft.Teams  -e
+winget install --id=JetBrains.IntelliJIDEA.Community  -e
